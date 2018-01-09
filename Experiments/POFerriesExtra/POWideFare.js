a321c45939e687291a19cb52ef205e053ceee4fc
Venask4 <mgcoloe@gmail.com>
2018-01-08 22:08:38 -0500

    make AWA changes

--- Experiments/POFerriesExtra/POWideFare.js
+++ Experiments/POFerriesExtra/POWideFare.js
@@ -24,7 +24,10 @@ var exp = (function($) {
 	exp.vars = {
 			fareDiv: '<div id="awa-fare-container"></div>',
 			sideDiv: '<div id="awa-side-container"></div>',
-			hLine: '<span class="awa-line"></span>'
+			hLine: '<span class="awa-line"></span>',
+			splitDiv: '<div class="awa-split"></div>',
+			passTitle: '<p>Passengers:</p>',
+			vehicleTitle: '<p>Vehicle:</p>'
 		};
 
 	// Styles
@@ -37,27 +40,30 @@ var exp = (function($) {
 			margin: 0 auto 12px;\
 		}\
 		#awa-fare-container {\
-			width: 30%;\
+			width: 55%;\
 			height: auto;\
-			margin-left: 42px;\
+			margin-left: 0px;\
 			z-index: 1;\
 			float: left;\
-			position: absolute;\
+			position: relative;\
 		}\
 		#awa-side-container {\
-			width: 100%;\
+			width: 45%;\
 			height: auto;\
-			display: block;\
+			display: inline-block;\
 		}\
 		.farefinder-container {\
 			top: 0;\
 		}\
 		#farefinder ul.nav {\
-			top: -33px;\
-			position: relative;\
+			top: 0px;\
+			position: absolute;\
 			width: 35%;\
 			background: white;\
 			border: 1px solid #ff8d02;\
+		}\
+		#fareFinderForm {\
+			margin-top: 10px;\
 		}\
 		.farefinder-container .farefinder {\
 			background-color: white;\
@@ -77,8 +83,6 @@ var exp = (function($) {
 			height: auto;\
 		}\
 		.farefinder-container .farefinder dl dd.ffreturning>div>ul li:first-child label, .farefinder-container .farefinder dl dd.ffwho>div>ul li:first-child label {\
-			background: none;\
-			padding-top: 0;\
 		}\
 		.blk.bt.ms500 {\
 			border: 1px solid #c4c2c2;\
@@ -88,29 +92,40 @@ var exp = (function($) {
 			box-shadow: none;\
 			border-bottom: none;\
 		}\
+		.awa-img {\
+			float: right;\
+			max-width: 940px;\
+		}\
+		.awa-banner {\
+			float: none;\
+		}\
 		';
 	exp.ferryCss = '\
+		.awa-sngl-radio {\
+			margin-right: 25px;\
+			float: right;\
+		}\
 		.awa-rtrn-radio {\
-			margin-left: 25px;\
+			margin-right: 25px;\
 			margin-top: 0 !important;\
+			float: right;\
 		}\
 		#ferrytab a:hover {\
 			background-color: #f4f4f4;\
 		}\
-		#returnJourneyComboBoxSelectBoxIt {\
-			z-index: -1;\
-		}\
 		#singleJourneyComboBoxSelectBoxItContainer {\
-			margin-right: 13px;\
+			float: none !important;\
+			display: block;\
+			margin-bottom: 10px;\
+		}\
+		#returnJourneyComboBoxSelectBoxItContainer {\
+			float: none !important;\
+			display: block;\
+			margin-bottom: 10px;\
 		}\
 		#where-ff {\
 			height: 42px;\
 		}\
-		#singleJourneyTimeComboBoxSelectBoxItContainer {\
-			z-index: -1;\
-		}\
-		#returnJourneyTimeComboBoxSelectBoxItContainer {\
-			z-index: -1;\
 		}\
 		.farefinder-container .farefinder dl dd input[type=text].hasDatepicker {\
 			width: 129px;\
@@ -131,22 +146,40 @@ var exp = (function($) {
 			margin-top: 10px;\
 		}\
 		.awa-vehicle-form {\
-			float: left;\
-			clear: both;\
+			display: block;\
 			width: 50%;\
 		}\
 		.awa-trailer-form {\
-			display: inline-block;\
+			display: block;\
 			width: 50%;\
 		}\
 		.awa-who-ul {\
-			margin-right: 10% !important;\
+			margin-right: 6% !important;\
 		}\
 		#trailerOutboundComboBoxSelectBoxItContainer {\
 			float: none !important;\
+			margin-top: 8px;\
 		}\
 		#vehicleTypeOutboundComboBoxSelectBoxItContainer {\
 			float: none !important;\
+			margin-top: 8px;\
+		}\
+		#singleJourneyComboBoxSelectBoxIt {\
+			width: 150px;\
+		}\
+		#returnJourneyComboBoxSelectBoxIt {\
+			width: 150px;\
+		}\
+		.awa-split {\
+			width: 50%;\
+			display: inline-block;\
+			vertical-align: top;\
+		}\
+		.farefinder-container .farefinder dl dd.ffreturning>div, .farefinder-container .farefinder dl dd.ffwho>div {\
+			padding-left: 0 !important;\
+		}\
+		#ou_passengersSelectWrapperDD {\
+			padding-left: 0 !important;\
 		}\
 		';
 
@@ -167,12 +200,11 @@ var exp = (function($) {
 		var $sideContainer = $('#awa-side-container');
 		$sideContainer.append($sideContent);
 
-		// Hide side container content
+		// Style side container content
 		$sideContainer.children('div').eq(0).hide();
-		$sideContainer.children('div').eq(1).hide();
-		$sideContainer.children('div').eq(2).hide();
-		$('.large-banner-badge.pa.po-headerblue').hide();
-		$sideContainer.find('img').css('width', '100%');
+		$fareContainer.before($sideContainer.children('div').eq(1).addClass('awa-banner'));
+		//$sideContainer.children('div').eq(2).hide();
+		//$('.large-banner-badge.pa.po-headerblue').hide();
 
 		// FERRY STYLE FUNCTION
 		function ferryStyle() {
@@ -180,31 +212,55 @@ var exp = (function($) {
 			$('head').append('<style>' + exp.ferryCss + '</style>');
 
 			// Re-arrange fare finder
-			var $returnRadio = $('.form-group.where-returnopt').children('div.radio')
+			$('#routeout').after($('#singleJourneyComboBoxSelectBoxItContainer'));
+			$('#routereturn').after($('#returnJourneyComboBoxSelectBoxItContainer'));
+			var $singleRadio = $('.form-group.where-singleopt').children('div.radio');
+			$singleRadio.addClass('awa-sngl-radio');
+			var $returnRadio = $('.form-group.where-returnopt').children('div.radio');
 			$returnRadio.addClass('awa-rtrn-radio');
-			$('.form-group.where-singleopt').children('div.radio').after($returnRadio);
-			var $vehicleForm = $('.form-group.vehicle').first();
-			var $trailerForm = $('.form-group.trailers').first();
-			$('#when-ff').append($vehicleForm).append($trailerForm);
-			$vehicleForm.addClass('awa-vehicle-form');
-			$trailerForm.addClass('awa-trailer-form');
+			$singleRadio.after($returnRadio);
+
+			// Add in split divs
+			$('#who-ff').prepend(exp.vars.splitDiv).prepend(exp.vars.splitDiv);
+			var $leftCol = $('.awa-split').eq(0);
+			var $rightCol = $('.awa-split').eq(1);
+
+			$leftCol.prepend($('#ou_passengersSelectWrapperDD'));
 			var $whoUls = $('.fareFinderSelectWrapper').children('ul');
 			$whoUls.each(function($whoUls) {
 				$(this).addClass('awa-who-ul');
 			})
+			$leftCol.prepend(exp.vars.passTitle);
+
+			var $vehicleForm = $('.form-group.vehicle').first();
+			var $trailerForm = $('.form-group.trailers').first();
+			$rightCol.prepend($vehicleForm);
+			$rightCol.prepend($trailerForm);
+			$vehicleForm.addClass('awa-vehicle-form');
+			$trailerForm.addClass('awa-trailer-form');
+			$rightCol.prepend(exp.vars.vehicleTitle);
+
 
 			// Hide unwanted titles
 			$('#where-ff').children('dt').first().hide();
 			$('#when-ff').children('dt').first().hide();
 			$('#who-ff').children('dt').first().hide();
 			$('#how-ff').hide();
-			$('.ico-vehicle').first().hide()
-			$('.ico-trailers').first().hide();
+			// $('.ico-vehicle').first().hide()
+			// $('.ico-trailers').first().hide();
 
 			// Change WHO lables
-			$('#adult').text('Adult 16+');
-			$('#child').text('Child 4-15');
-			$('#toddler').text('Infant 0-3');
+			// $('#adult').text('Adult 16+');
+			// $('#child').text('Child 4-15');
+			// $('#toddler').text('Infant 0-3');
+
+			// Add second set of split divs
+			$('#samePassengerCheckBoxWrapper').prepend(exp.vars.splitDiv).prepend(exp.vars.splitDiv);
+			var $botLeftCol = $('.awa-split').eq(2);
+			var $botRightCol = $('.awa-split').eq(3);
+
+			$botLeftCol.prepend($('.no-background.discount-code'));
+			$botRightCol.prepend($('.return-party.clearfix')).prepend($('.getaquotebtn'));
 
 			// Fix extra ad
 			$fareContainer.find('div.c_46').hide();
@@ -213,6 +269,17 @@ var exp = (function($) {
 			$('#where-ff').after(exp.vars.hLine);
 			$('#when-ff').after(exp.vars.hLine);
 			$('#who-ff').after(exp.vars.hLine);
+
+			// Fix img height
+			var imgArrayLength = $sideContainer.find('img').length
+			if (imgArrayLength > 1) {
+				$sideContainer.find('img').eq(imgArrayLength - 1).addClass('awa-img');	
+			}
+			else {
+				$sideContainer.find('img').first().addClass('awa-img');
+			}
+			var imgHeight = parseInt($fareContainer.css('height')) - 85 + 'px';
+			$('.awa-img').css('height', imgHeight);
 		}
 		ferryStyle();
 
