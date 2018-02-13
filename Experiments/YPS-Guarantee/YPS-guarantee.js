54d42fa3eb8db7d02e601c298b9310ba67141839
Venask4 <mgcoloe@gmail.com>
2018-02-12 21:37:28 -0500

    add modal

--- Experiments/YPS-Guarantee/YPS-guarantee.js
+++ Experiments/YPS-Guarantee/YPS-guarantee.js
@@ -21,6 +21,7 @@ var exp = (function($) {
 	// Variables
 	// Object containing variables, generally these would be strings or jQuery objects
 	exp.vars = {
+		awaModal: '<div id="awa-modal"><div id="awa-modal-content"><span class="awa-close">&times;&nbsp;</span><br><br><div class="awa-title-container"><h3>Your space is for you and only you to use for the duration of your booking and your host knows this.</h3><h2 class="awa-close-txt">OK, got it!</h2></div></div></div>',
 	};
 
 	// Styles
@@ -31,26 +32,34 @@ var exp = (function($) {
     		font-weight: 400;\
     		font-family: Montserrat;\
 		}\
-		.awa-tooltip {\
-		    background-color: #679bef;\
-		    border-radius: 50%;\
-		    width: 15px;\
-		    display: inline-block;\
-		    color: white;\
-		    font-weight: bold;\
-		    padding-right: 6px;\
-		    height: 0px;\
-		    font-family: Montserrat;\
-		    padding-bottom: 15px;\
-		    font-size: 11px;\
-		    line-height: 16px;\
+		.awa-img {\
+			height: 15px;\
+		}\
+		#awa-modal {\
+			display: none;\
+    		position: fixed;\
+    		z-index: 1;\
+    		left: 0;\
+    		top: 0;\
+    		width: 100%;\
+    		height: 100%;\
+     		background-color: rgba(0,0,0,0.4);\
+		}\
+		#awa-modal-content {\
+			width: 800px;\
+			height: auto;\
+			background-color: white;\
+			margin: 15% auto;\
+			display: block;\
+			padding: 0 0 0 40px;\
+			border-radius: 10px;\
 		}\
 	';
 
 	// Init function
 	// Called to run the actual experiment, DOM manipulation, event listeners, etc
 	exp.init = function() {
-		// Add styles
+		// // Add styles
 		$('head').append('<style>' + exp.css + '</style>');
 
 		// Create Object Array
@@ -63,8 +72,34 @@ var exp = (function($) {
 		// Add text
 		$.each($divs, function(){
 			$(this).addClass('awa-text');
-			$(this).html('<span>Guaranteed </span><span class="awa-tooltip">i</span>');
+			$(this).html('<div class="awa-guarantee"><span>Guaranteed  </span><img class="awa-img" data-v-995a4c6e="" src="/images/info-colour.png?8a209c06ec7a56d81233d00ddfdb240f" srcset="/images/info-colour@2x.png?89ce35c2dc495ea257c0501ec7965c4e2x" class="icon-image" data-original-title="" title=""></div>');
 		});
+
+		// Add in and display modal
+		$('body').append(exp.vars.awaModal);
+		var $awaModal = $('#awa-modal');
+		var $closeButton = $('.awa-close');
+		var $awaModalContent = $('#awa-modal-content');
+
+		$('.awa-guarantee').on('click', function() {
+			$('#awa-modal').show();
+			console.log('ran');
+		})
+
+		   // Close functions
+    	$closeButton.on('click', function() {
+		    $awaModal.css('display', 'none');
+		})
+
+		$closeButton.on('click', function() {
+		    $('.awa-close-txt').css('display', 'none');
+		})
+
+		// $(document).click(function(event) { 
+  //   		if(!$(event.target).closest($awaModalContent).length && $awaModal.css('display') === 'block') {
+  //   			$awaModal.css('display', 'none');
+  //   		}
+  //   	})
 	};
 
 	exp.init();
